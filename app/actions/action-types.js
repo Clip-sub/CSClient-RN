eff88c5d70e9038686a9ac9548cbb56dbc3e29c9
QuanPV <quanpv@ows.vn>
2017-02-22 01:24:27 +0700

    Minor update

--- app/actions/action-types.js
+++ app/actions/action-types.js
@@ -2,4 +2,7 @@
 export const LOAD_POSTS = 'LOAD_POSTS';
 export const LOAD_POST = 'LOAD_POST';
 
+export const SUBMIT_COMMENT = 'SUBMIT_COMMENT';
+export const UPDATE_PROFILE = 'UPDATE_PROFILE';
+
 export const CHANGE_PATH = 'CHANGE_PATH';
