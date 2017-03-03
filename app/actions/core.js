e8dc82d89de1e02260a0d78fa7beffe502017af6
QuanPV <quanpv@ows.vn>
2017-03-03 08:36:15 +0700

    Added some apis

---
+++ app/actions/core.js
@@ -0,0 +1,22 @@
+'use strict';
+import Types from "./action-types";
+
+export function getPosts(page: number, perPage: ?number = 1) {
+  return dispatch => {
+    return API.loadPosts;
+  }
+}
+
+export function getRecentPosts(page: number, perPage: ?number = 1) {
+  return dispatch => {
+    return API.getRecentPosts(page, perPage)
+      .then(response => dispatch(gotRecentPosts(response)))
+      .catch(error => dispatch(gotError(error)));
+  }
+}
+
+export function getCurrentUserInfo(id: number) {
+  return dispatch => {
+    
+  }
+}
